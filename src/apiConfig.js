09ea03f643e7dc8fa87d3431f939c0da20ab557c
srsexton94 <srsexton94@gmail.com>
2020-03-30 11:24:07 -0400

    Add correct production url to apiConfig

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://fast-oasis-63373.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
