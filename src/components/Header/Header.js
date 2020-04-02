045bdf9dfbc292fa2e336231abd04362a9468274
srsexton94 <srsexton94@gmail.com>
2020-04-02 12:37:44 -0400

    Style adjustments
    Add shadows and adjust header nav text color

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,23 +4,23 @@ import Navbar from 'react-bootstrap/Navbar'
 
 const authenticatedOptions = (
   <Fragment>
-    <Nav.Link href="#create-post">Create a Post</Nav.Link>
-    <Nav.Link href="#change-password">Change Password</Nav.Link>
-    <Nav.Link href="#sign-out">Sign Out</Nav.Link>
+    <Nav.Link className="header-link" href="#create-post">Create a Post</Nav.Link>
+    <Nav.Link className="header-link" href="#change-password">Change Password</Nav.Link>
+    <Nav.Link className="header-link" href="#sign-out">Sign Out</Nav.Link>
   </Fragment>
 )
 
 const unauthenticatedOptions = (
   <Fragment>
-    <Nav.Link href="#sign-up">Sign Up</Nav.Link>
-    <Nav.Link href="#sign-in">Sign In</Nav.Link>
+    <Nav.Link className="header-link" href="#sign-up">Sign Up</Nav.Link>
+    <Nav.Link className="header-link" href="#sign-in">Sign In</Nav.Link>
   </Fragment>
 )
 
 const alwaysOptions = (
   <Fragment>
-    <Nav.Link href="#/">Home</Nav.Link>
-    <Nav.Link href="#posts">Posts</Nav.Link>
+    <Nav.Link className="header-link" href="#/">Home</Nav.Link>
+    <Nav.Link className="header-link" href="#posts">Posts</Nav.Link>
   </Fragment>
 )
 
