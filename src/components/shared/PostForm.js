7438e2b64e9c84c50edd337ca0f6597038543bee
srsexton94 <srsexton94@gmail.com>
2020-05-26 22:02:00 -0400

    Edit the Create/Edit view forms styling

--- src/components/shared/PostForm.js
+++ src/components/shared/PostForm.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
 // creates a component for the create/edit form
 const PostForm = ({ post, handleSubmit, handleChange, cancelPath }) => {
   // declares an array of the different post 'types' (to match sort routes)
@@ -21,9 +24,9 @@ const PostForm = ({ post, handleSubmit, handleChange, cancelPath }) => {
     // input takes a conditional to avoid oscillating b/w `controlled` and
     // `uncontrolled` input types (receives React warning)
     return (
-      <fieldset key={type}>
+      <Form.Group className="radio" key={type}>
         <label htmlFor={type}>{type.charAt(0).toUpperCase() + type.slice(1)}</label>
-        <input
+        <Form.Control
           type="radio"
           id={type}
           value={type || ''}
@@ -31,54 +34,61 @@ const PostForm = ({ post, handleSubmit, handleChange, cancelPath }) => {
           onChange={handleChange}
           checked={isChecked}
         />
-      </fieldset>
+      </Form.Group>
     )
   })
 
   // returns JSX for the `PostForm` component, includes an input for each
   // attribute of `post` resource
   return (
-    <form className="post-form" onSubmit={handleSubmit}>
-      <fieldset>
-        <label>Title: </label>
-        <input
+    <Form className="post-form" onSubmit={handleSubmit}>
+      <Form.Group className="main-group">
+        <label className="main-label">Title: </label>
+        <Form.Control
+          required
           type="text"
           placeholder="A descriptive title"
           value={post.title}
           name="title"
           onChange={handleChange}
         />
-      </fieldset>
+      </Form.Group>
 
-      <fieldset>
-        <label>Body Text: </label>
-        <textarea
+      <Form.Group className="main-group">
+        <label className="main-label">Body Text: </label>
+        <Form.Control
+          as="textarea"
           value={post.text}
           name="text"
           onChange={handleChange}
           rows="5"
           cols="50"
         />
-      </fieldset>
+      </Form.Group>
 
-      <fieldset>
-        <label>Resource Link: </label>
-        <input
+      <Form.Group className="main-group">
+        <label className="main-label">Resource Link: </label>
+        <Form.Control
+          required
           type="text"
           placeholder="provide a URL"
           value={post.sourceUrl}
           name="sourceUrl"
           onChange={handleChange}
         />
-      </fieldset>
+      </Form.Group>
 
-      <fieldset className="radios">
-        {typeJSX}
-      </fieldset>
+      <Form.Group className="main-group">
+        <label className="main-label">Type of resource?</label>
+        <Form.Group className="radios">
+          {typeJSX}
+        </Form.Group>
+      </Form.Group>
 
-      <fieldset>
-        <label>Location (Zip Code)</label>
-        <input
+      <Form.Group className="main-group">
+        <label className="main-label">Location:</label>
+        <Form.Control
+          required
           type="text"
           placeholder="enter zip code"
           value={post.zip}
@@ -86,13 +96,30 @@ const PostForm = ({ post, handleSubmit, handleChange, cancelPath }) => {
           pattern="[0-9]*"
           onChange={handleChange}
         />
-      </fieldset>
+        <Form.Text className="text-muted">
+          If it&apos;s a regional resource, try to approximate.
+        </Form.Text>
+        <Form.Text className="text-muted">
+          If you don&apos;t know, or it&apos;s remote, please feel free to enter `0`.
+        </Form.Text>
+      </Form.Group>
 
-      <button style={{ margin: '0 2px 10px' }} className="btn btn-secondary" type="submit">Submit</button>
+      <Button
+        variant="info"
+        type="submit"
+        size="lg"
+      >
+        Submit
+      </Button>
       <Link to={cancelPath}>
-        <button style={{ margin: '0 2px 10px' }} className="btn btn-secondary" >Cancel</button>
+        <Button
+          variant="secondary"
+          size="lg"
+        >
+          Cancel
+        </Button>
       </Link>
-    </form>
+    </Form>
   )
 }
 
